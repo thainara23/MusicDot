b62fb9bdd9306e1aff8a880a69df731aeb9e0d77
thainara23 <90209904+thainara23@users.noreply.github.com>
2021-11-26 22:31:31 -0300

    musicdot

---
+++ js/matricula.js
@@ -0,0 +1,31 @@
+var $tdTotalCursos = document.querySelector('.js-total-de-cursos')
+var $tdTotalDeHoras = document.querySelector('.js-total-de-horas')
+var $buttonConfirmar = document.querySelector('.js-botao-matricula')
+
+var TotalHoras = 0
+var TotalCursos = 0
+
+function adicionaCurso(checkbox) {
+    if(checkbox.checked){
+        TotalCursos ++
+        TotalHoras += parseInt(checkbox.value)
+    }
+    else{
+        TotalCursos --
+        TotalHoras -= parseInt(checkbox.value)
+    }
+
+    $tdTotalDeHoras.textContent = TotalHoras + 'h'
+    $tdTotalCursos.textContent = TotalCursos + ' cursos(s)'
+}
+
+$buttonConfirmar.onclik = confirmaMatriculas()
+
+function confirmaMatriculas() {
+    if(TotalCursos == 0) {
+        alert('Nenhum curso selecionado')
+    } else {
+        alert('Matricula confirmada nos cursos!')
+        window.location.href = 'index.html'
+    }
+}
